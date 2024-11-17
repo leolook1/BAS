ffd43ae9db597096a71f2371cd3903194f5b8b6d
leolook1 <126547557+leolook1@users.noreply.github.com>
2024-11-17 23:05:30 +0800

    Create metric.py

---
+++ metric.py
@@ -0,0 +1,173 @@
+
+import numpy as np
+from scipy.ndimage import convolve, distance_transform_edt as bwdist
+
+
+
+_TYPE = np.float64
+
+
+def _prepare_data(pred: np.ndarray, gt: np.ndarray) -> tuple:
+    gt = gt > 128
+    pred = pred / 255
+    if pred.max() != pred.min():
+        pred = (pred - pred.min()) / (pred.max() - pred.min())
+    return pred, gt
+
+
+def _get_adaptive_threshold(matrix: np.ndarray, max_value: float = 1) -> float:
+    return min(2 * matrix.mean(), max_value)
+
+
+class F1measure(object):
+    def __init__(self, beta: float = 0.3):
+        self.beta = beta
+        self.precisions = []
+        self.recalls = []
+        self.adaptive_fms = []
+        self.changeable_fms = []
+
+    def step(self, pred: np.ndarray, gt: np.ndarray):
+        pred, gt = _prepare_data(pred, gt)
+
+        adaptive_fm = self.cal_adaptive_fm(pred=pred, gt=gt)
+        self.adaptive_fms.append(adaptive_fm)
+
+        precisions, recalls, changeable_fms = self.cal_pr(pred=pred, gt=gt)
+        self.precisions.append(precisions)
+        self.recalls.append(recalls)
+        self.changeable_fms.append(changeable_fms)
+
+    def cal_adaptive_fm(self, pred: np.ndarray, gt: np.ndarray) -> float:
+        adaptive_threshold = _get_adaptive_threshold(pred, max_value=1)
+        binary_predcition = pred >= adaptive_threshold
+        area_intersection = binary_predcition[gt].sum()
+        if area_intersection == 0:
+            adaptive_fm = 0
+        else:
+            pre = area_intersection / np.count_nonzero(binary_predcition)
+            rec = area_intersection / np.count_nonzero(gt)
+            adaptive_fm = 2* pre * rec / (pre + rec)
+        return adaptive_fm
+
+    def cal_pr(self, pred: np.ndarray, gt: np.ndarray) -> tuple:
+        bins = np.linspace(0, 256, 257)
+        fg_hist, _ = np.histogram(pred[gt], bins=bins)  
+        bg_hist, _ = np.histogram(pred[~gt], bins=bins)
+        fg_w_thrs = np.cumsum(np.flip(fg_hist), axis=0)
+        bg_w_thrs = np.cumsum(np.flip(bg_hist), axis=0)
+        TPs = fg_w_thrs
+        Ps = fg_w_thrs + bg_w_thrs
+        Ps[Ps == 0] = 1
+        T = max(np.count_nonzero(gt), 1)
+        # Ps[i] = 0 -> fg_w_thrs[i] = 0, bg_w_thrs[i] = 0
+        precisions = TPs / Ps
+        recalls = TPs / T
+
+        numerator = 2 * precisions * recalls
+        denominator = np.where(numerator == 0, 1, 1 * precisions + recalls)
+        changeable_fms = numerator / denominator
+        return precisions, recalls, changeable_fms
+
+    def get_results(self) -> dict:
+        adaptive_fm = np.mean(np.array(self.adaptive_fms, _TYPE))
+        changeable_fm = np.mean(np.array(self.changeable_fms, dtype=_TYPE), axis=0)
+        precision = np.mean(np.array(self.precisions, dtype=_TYPE), axis=0)  # N, 256
+        recall = np.mean(np.array(self.recalls, dtype=_TYPE), axis=0)  # N, 256
+        return dict(fm=dict(adp=adaptive_fm, curve=changeable_fm),
+                    pr=dict(p=precision, r=recall))
+
+
+class MAE(object):
+    def __init__(self):
+        self.maes = []
+
+    def step(self, pred: np.ndarray, gt: np.ndarray):
+        pred, gt = _prepare_data(pred, gt)
+
+        mae = self.cal_mae(pred, gt)
+        self.maes.append(mae)
+
+    def cal_mae(self, pred: np.ndarray, gt: np.ndarray) -> float:
+        mae = np.mean(np.abs(pred - gt))
+        return mae
+
+    def get_results(self) -> dict:
+        mae = np.mean(np.array(self.maes, _TYPE))
+        return dict(mae=mae)
+
+
+
+class WeightedFmeasure(object):
+    def __init__(self, beta: float = 1):
+        self.beta = beta
+        self.weighted_fms = []
+
+    def step(self, pred: np.ndarray, gt: np.ndarray):
+        pred, gt = _prepare_data(pred=pred, gt=gt)
+
+        if np.all(~gt):
+            wfm = 0
+        else:
+            wfm = self.cal_wfm(pred, gt)
+        self.weighted_fms.append(wfm)
+
+    def cal_wfm(self, pred: np.ndarray, gt: np.ndarray) -> float:
+        # [Dst,IDXT] = bwdist(dGT);
+        Dst, Idxt = bwdist(gt == 0, return_indices=True)
+
+        # %Pixel dependency
+        # E = abs(FG-dGT);
+        E = np.abs(pred - gt)
+        # Et = E;
+        # Et(~GT)=Et(IDXT(~GT)); %To deal correctly with the edges of the foreground region
+        Et = np.copy(E)
+        Et[gt == 0] = Et[Idxt[0][gt == 0], Idxt[1][gt == 0]]
+
+        # K = fspecial('gaussian',7,5);
+        # EA = imfilter(Et,K);
+        K = self.matlab_style_gauss2D((7, 7), sigma=5)
+        EA = convolve(Et, weights=K, mode="constant", cval=0)
+        # MIN_E_EA = E;
+        # MIN_E_EA(GT & EA<E) = EA(GT & EA<E);
+        MIN_E_EA = np.where(gt & (EA < E), EA, E)
+
+        # %Pixel importance
+        # B = ones(size(GT));
+        # B(~GT) = 2-1*exp(log(1-0.5)/5.*Dst(~GT));
+        # Ew = MIN_E_EA.*B;
+        B = np.where(gt == 0, 2 - np.exp(np.log(0.5) / 5 * Dst), np.ones_like(gt))
+        Ew = MIN_E_EA * B
+
+        # TPw = sum(dGT(:)) - sum(sum(Ew(GT)));
+        # FPw = sum(sum(Ew(~GT)));
+        TPw = np.sum(gt) - np.sum(Ew[gt == 1])
+        FPw = np.sum(Ew[gt == 0])
+
+        # R = 1- mean2(Ew(GT)); %Weighed Recall
+        # P = TPw./(eps+TPw+FPw); %Weighted Precision
+        R = 1 - np.mean(Ew[gt == 1])
+        P = TPw / (TPw + FPw + _EPS)
+
+        # % Q = (1+Beta^2)*(R*P)./(eps+R+(Beta.*P));
+        Q = (1 + self.beta) * R * P / (R + self.beta * P + _EPS)
+
+        return Q
+
+    def matlab_style_gauss2D(self, shape: tuple = (7, 7), sigma: int = 5) -> np.ndarray:
+        """
+        2D gaussian mask - should give the same result as MATLAB's
+        fspecial('gaussian',[shape],[sigma])
+        """
+        m, n = [(ss - 1) / 2 for ss in shape]
+        y, x = np.ogrid[-m: m + 1, -n: n + 1]
+        h = np.exp(-(x * x + y * y) / (2 * sigma * sigma))
+        h[h < np.finfo(h.dtype).eps * h.max()] = 0
+        sumh = h.sum()
+        if sumh != 0:
+            h /= sumh
+        return h
+
+    def get_results(self) -> dict:
+        weighted_fm = np.mean(np.array(self.weighted_fms, dtype=_TYPE))
+        return dict(wfm=weighted_fm)
